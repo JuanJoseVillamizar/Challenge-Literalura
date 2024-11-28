6e658b89a1033956207f8413697c49d4978200a5
JuanJoseVillamizar <juanjvs1419@gmail.com>
2024-11-28 15:46:24 -0500

    feat: add validateText method to ensure non-empty text input

--- src/main/java/com/JuanJose/LiterAlura/util/TextUtils.java
+++ src/main/java/com/JuanJose/LiterAlura/util/TextUtils.java
@@ -1,4 +1,12 @@
 package com.JuanJose.LiterAlura.util;
 
 public class TextUtils {
+    private TextUtils(){
+
+    }
+    public static void validateText (String text){
+        if (text == null || text.trim().isEmpty()) {
+            throw new IllegalArgumentException("Text cannot be null or empty");
+        }
+    }
 }
