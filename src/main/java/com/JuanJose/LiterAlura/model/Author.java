02439307c2a6e3e8839e7d9ebd83024a39ad412a
JuanJoseVillamizar <juanjvs1419@gmail.com>
2024-12-01 21:07:31 -0500

    refactor: update Author class to use AuthorDTO and improve null handling

--- src/main/java/com/JuanJose/LiterAlura/model/Author.java
+++ src/main/java/com/JuanJose/LiterAlura/model/Author.java
@@ -1,7 +1,9 @@
 package com.JuanJose.LiterAlura.model;
 
+import com.JuanJose.LiterAlura.dto.AuthorDTO;
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,8 +13,8 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Integer birth_year;
-    private Integer death_year;
+    private Integer birthYear;
+    private Integer deathYear;
     private String name;
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
     @JoinTable(
@@ -20,34 +22,34 @@ public class Author {
             joinColumns = @JoinColumn(name = "author_id"),
             inverseJoinColumns = @JoinColumn(name= "book_id")
     )
-    private List<Book> books;
+    private List<Book> books ;
     @Version  // Campo para la gestión de versiones en optimista
     private Long version;
 
-    public Author(){
+    public Author(){}
 
-    }
-    public Author(Integer birth_year, Integer death_year, String name, List<Book> books) {
-        this.birth_year = birth_year;
-        this.death_year = death_year;
-        this.name = name;
-        this.books = books;
-    }
-
-    public Integer getBirth_year() {
-        return birth_year;
+    public Author(AuthorDTO authorDTO, List<Book> books) {
+        this.birthYear = (authorDTO.birth_year() != null ? authorDTO.birth_year() : null);
+        this.deathYear = (authorDTO.death_year() != null ? authorDTO.death_year() : null);
+        this.name = authorDTO.name();
+        this.books = books != null ? books : new ArrayList<>();
     }
 
-    public void setBirth_year(Integer birth_year) {
-        this.birth_year = birth_year;
+
+    public Integer getBirthYear() {
+        return birthYear;
     }
 
-    public Integer getDeath_year() {
-        return death_year;
+    public void setBirthYear(Integer birth_year) {
+        this.birthYear = birth_year;
     }
 
-    public void setDeath_year(Integer death_year) {
-        this.death_year = death_year;
+    public Integer getDeathYear() {
+        return deathYear;
+    }
+
+    public void setDeathYear(Integer death_year) {
+        this.deathYear = death_year;
     }
 
     public String getName() {
@@ -69,10 +71,10 @@ public class Author {
     @Override
     public String toString() {
         return "---------Author------------------------- \n" +
-                "birth_year: " + birth_year +  "\n" +
-                "death_year: " + death_year +  "\n" +
                 "name:" + name  + "\n" +
+                "birth_year: " + birthYear + "\n" +
+                "death_year: " + deathYear + "\n" +
                 "books: " + (books != null ? books.stream().map(Book::getTitle).collect(Collectors.joining(", ")) : "No books" ) +  "\n" +
-                "----------------------------------------";
+                " -------------------------------------- ";
     }
 }
