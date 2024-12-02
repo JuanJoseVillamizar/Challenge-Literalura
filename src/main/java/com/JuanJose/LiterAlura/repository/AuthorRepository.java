615478830070451e6469cba5a396965b21532a6e
JuanJoseVillamizar <juanjvs1419@gmail.com>
2024-12-02 11:26:16 -0500

    feat: add AuthorRepository with custom query methods

--- src/main/java/com/JuanJose/LiterAlura/repository/AuthorRepository.java
+++ src/main/java/com/JuanJose/LiterAlura/repository/AuthorRepository.java
@@ -1,4 +1,22 @@
 package com.JuanJose.LiterAlura.repository;
 
-public interface AuthorRepository {
+import com.JuanJose.LiterAlura.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    //Find Author by name (case-insensitive)
+    Optional<Author> findAuthorByNameContainingIgnoreCase(String name);
+
+    // Find Authors by name fragment (case-insensitive)
+    List<Author> findListAuthorsByNameContainingIgnoreCase(String name);
+
+    // Find authors who wre alive in a given year
+    @Query(value = "SELECT a FROM Author a WHERE a.birthYear < :year AND a.deathYear > :year")
+    List<Author> findAuthorsAliveInYear(int year);
 }
