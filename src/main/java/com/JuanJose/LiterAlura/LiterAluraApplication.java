3cefcafa884e6ee0fd2ba951a531e988548c0abb
JuanJoseVillamizar <juanjvs1419@gmail.com>
2024-12-02 15:05:26 -0500

    refactor: update LiterAluraApplication to inject AuthorService and AuthorRepository

--- src/main/java/com/JuanJose/LiterAlura/LiterAluraApplication.java
+++ src/main/java/com/JuanJose/LiterAlura/LiterAluraApplication.java
@@ -1,7 +1,9 @@
 package com.JuanJose.LiterAlura;
 
 import com.JuanJose.LiterAlura.app.App;
+import com.JuanJose.LiterAlura.repository.AuthorRepository;
 import com.JuanJose.LiterAlura.repository.BookRepository;
+import com.JuanJose.LiterAlura.service.AuthorService;
 import com.JuanJose.LiterAlura.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -12,9 +14,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LiterAluraApplication implements CommandLineRunner {
 
 	@Autowired
-	private BookRepository repository;
+	private BookRepository bookRepository;
 	@Autowired
 	private BookService bookService;
+	@Autowired
+	private AuthorRepository authorRepository;
+	@Autowired
+	private AuthorService authorService;
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
 	}
@@ -22,7 +28,7 @@ public class LiterAluraApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-	App app = new App(repository,bookService);
+	App app = new App(bookRepository,bookService,authorRepository,authorService);
 	app.showMenu();
 	}
 }
